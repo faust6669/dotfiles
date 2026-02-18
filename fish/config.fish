e31d3f9953cc04ae7c1c96579ed430a91739030e
WarPig <davidhesson1006@gmail.com>
2026-02-18 04:58:17 -0500

    Update configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
+ #overwrite greeting
+
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
