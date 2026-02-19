8037c4cf065504967961d93c93d193afb7ac720c
WarPig <davidhesson1006@gmail.com>
2026-02-18 23:57:10 -0500

    Update configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,40 +1,33 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+  # 1. Load Distro Defaults
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
 
- #overwrite greeting
-
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-# Inside ~/.config/fish/config.fish
+# 2. PATHS
 if test -d ~/.config/emacs/bin
     set -gx PATH $PATH ~/.config/emacs/bin
 end
 
-
+# 3. ALIASES
 alias dsync='~/.emacs.d/bin/doom sync'
-
-if status is-interactive
-    # 1. THE TILDE SHORTCUT (For your 60% board)
-    abbr -a tt '~'
-    
-    # 2. NAVIGATION SHORTCUTS
-    
-    
-    # 3. CACHYOS / ARCH UPDATES
-    abbr -a update 'sudo pacman -Syu'
-
-     # 3.5 supposedly auto saves on gh
-  abbr dots 'cd ~/dotfiles && git add . && git commit -m "Update configs" && git push && cd -'
-   
-    # 4. REMOVE THE GREETING (Optional - makes terminal cleaner)
-    set -g fish_greeting ""
-end
-# Force Arch logo and ignore system defaults
 alias fastfetch="fastfetch -c ~/.config/fastfetch/config.jsonc --logo arch"
-fastfetch
 
+# 4. THE NUCLEAR OPTION: Redefine the greeting function
+# This overwrites whatever CachyOS is trying to do.
+function fish_greeting
+    if status is-interactive
+        # This is the ONLY place fastfetch will run
+        command fastfetch -c ~/.config/fastfetch/config.jsonc --logo arch
+    end
+end
+
+# 5. ABBREVIATIONS
+if status is-interactive
+    abbr -a tt '~'
+    abbr -a --position anywhere tt '~'
+    abbr -a update 'sudo pacman -Syu'
+    abbr dots 'cd ~/dotfiles && git add . && git commit -m "Update configs" && git push && cd -'
+end
+
+# 6. PROMPT
 starship init fish | source
-
-
-abbr -a --position anywhere tt '~'
